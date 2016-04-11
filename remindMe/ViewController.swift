2d7e6c0842ec0706aefe45825bed7beeb4ae2cd5
Duane <duane.stoltz@gmail.com>
2016-04-11 07:14:28 +0200

    Initial Commit

---
+++ remindMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  remindMe
+//
+//  Created by Duane Stoltz on 11/04/2016.
+//  Copyright © 2016 Duane Stoltz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

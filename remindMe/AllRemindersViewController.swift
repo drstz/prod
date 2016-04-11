5ab74c077bebfe765e6df5ac7b09bfb9e48a9704
Duane <duane.stoltz@gmail.com>
2016-04-11 09:38:43 +0200

    Added tableView and fake data

--- remindMe/AllRemindersViewController.swift
+++ remindMe/AllRemindersViewController.swift
@@ -9,9 +9,20 @@
 import UIKit
 
 class AllRemindersViewController: UIViewController {
+    
+    // MARK: - Instance Variables
+    
+    var reminders = [String]()
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - The view
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateList()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +30,41 @@ class AllRemindersViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: - Reminder list
+    
+    func updateList() {
+        reminders = [String]()
+        
+        for i in 0...4 {
+            reminders.append(String(format: "Reminder #%d", i))
+        }
+        tableView.reloadData()
+    }
+}
 
+// MARK: - Extensions
 
+extension AllRemindersViewController: UITableViewDataSource {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return reminders.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cellIdentifier = "SearchResultCell"
+        
+        var cell: UITableViewCell! = tableView.dequeueReusableCellWithIdentifier(cellIdentifier)
+        if cell == nil {
+            cell = UITableViewCell(style: .Default, reuseIdentifier: cellIdentifier)
+        }
+        
+        cell.textLabel!.text = reminders[indexPath.row]
+        return cell
+    }
+    
+}
+
+extension AllRemindersViewController: UITableViewDelegate {
+    
 }
 
---
+++ remindMe/Reminder.swift
@@ -0,0 +1,9 @@
+//
+//  Reminder.swift
+//  remindMe
+//
+//  Created by Duane Stoltz on 11/04/2016.
+//  Copyright © 2016 Duane Stoltz. All rights reserved.
+//
+
+import Foundation

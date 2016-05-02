e21d06cfdeb9ec84cf53176717f0a64e7b68a1df
Duane <duane.stoltz@gmail.com>
2016-05-02 08:00:40 +0200

    fetch reminder from the reminderID in notification

--- remindMe/AppDelegate.swift
+++ remindMe/AppDelegate.swift
@@ -146,11 +146,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             if identifier == "HELLO" {
                 
                 print("You pressed the action")
-//                let navigationController = window!.rootViewController as! UINavigationController
-//                let navigationViewControllers = navigationController.viewControllers
-//                let allRemindersViewController = navigationViewControllers[0] as! AllRemindersViewController
+
                
-                //NSNotificationCenter.defaultCenter().postNotificationName("printHello", object: nil)
+                
+                
+                let idFromNotification = notification.userInfo!["ReminderID"] as! Int
+                print(idFromNotification)
+                
+                let fetchRequest = NSFetchRequest(entityName: "Reminder")
+                let predicate = NSPredicate(format: "%K == %@", "idNumber", "\(idFromNotification)" )
+                fetchRequest.predicate = predicate
+                
+                do {
+                    let result = try self.managedObjectContext.executeFetchRequest(fetchRequest)
+                    let reminder = result[0] as! NSManagedObject as! Reminder
+                    print(reminder.name)
+                    let navigationController = window!.rootViewController as! UINavigationController
+                    let navigationViewControllers = navigationController.viewControllers
+                    let allRemindersViewController = navigationViewControllers[0] as! AllRemindersViewController
+                    allRemindersViewController.reminderFromNotification = reminder
+                    NSNotificationCenter.defaultCenter().postNotificationName("printHello", object: nil)
+                } catch {
+                    let fetchError = error as NSError
+                    print(fetchError)
+                }
+                
+                
+                
             }
         }
         completionHandler()

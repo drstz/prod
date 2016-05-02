68d0df5ea77eeb5c861fc572e3bd080316fb3f80
Duane <duane.stoltz@gmail.com>
2016-05-02 07:15:49 +0200

    use userDefaults to take care of first time use

---
+++ remindMe/UserDefaults.swift
@@ -0,0 +1,28 @@
+//
+//  userDefaults.swift
+//  remindMe
+//
+//  Created by Duane Stoltz on 02/05/2016.
+//  Copyright © 2016 Duane Stoltz. All rights reserved.
+//
+
+import Foundation
+    
+func registerDefaults() {
+    let dictionary = ["FirstTime": true]
+    
+    NSUserDefaults.standardUserDefaults().registerDefaults(dictionary)
+}
+
+func isFirstTime() -> Bool {
+    let userDefaults = NSUserDefaults.standardUserDefaults()
+    let firstTime = userDefaults.boolForKey("FirstTime")
+    if firstTime {
+        userDefaults.setBool(false, forKey: "FirstTime")
+        userDefaults.synchronize()
+        return true
+    }
+    return false
+}
+
+

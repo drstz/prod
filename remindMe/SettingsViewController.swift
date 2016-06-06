2884521438ec0bc066d862663fb419dfed7d928f
Duane <duane.stoltz@gmail.com>
2016-06-06 12:48:54 +0200

    basic settings - no functionality

---
+++ remindMe/SettingsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SettingsViewController.swift
+//  remindMe
+//
+//  Created by Duane Stoltz on 06/06/2016.
+//  Copyright © 2016 Duane Stoltz. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UITableViewController {
+    
+
+}

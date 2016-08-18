0d9702d005fe38ae6ec876707c32b871eda3615b
Duane <duane.stoltz@gmail.com>
2016-08-18 08:56:24 +0200

    clear tabor delegate

--- remindMe/AllRemindersViewController+TabBarDelegate.swift
+++ remindMe/AllRemindersViewController+TabBarDelegate.swift
@@ -14,41 +14,11 @@ extension AllRemindersViewController: UITabBarControllerDelegate  {
     
     func tabBarController(tabBarController: UITabBarController, shouldSelectViewController viewController: UIViewController) -> Bool {
         print(#function)
-        let selectedIndex = myTabBarController.selectedIndex
-        
-        let navigationController = viewController as! UINavigationController
-        let viewControllers = navigationController.viewControllers
-        let allRemindersViewController = viewControllers[0] as! AllRemindersViewController
-        
-        let selectedViewControllerTag = allRemindersViewController.tabBarController?.tabBar.selectedItem?.tag
-        print("CHANGING TAB \(selectedIndex) ---> \(selectedViewControllerTag!) ")
-        print("Segment is \(segmentedControl.selectedSegmentIndex)")
-        
-        
-        let messageToSend = "I came from tab \(selectedIndex)"
-        allRemindersViewController.sentMessage = messageToSend
-        
-        
-        allRemindersViewController.managedObjectContext = managedObjectContext
-        allRemindersViewController.list = list
-        tabBarController.delegate = allRemindersViewController
-        allRemindersViewController.myTabBarController = tabBarController
-        
-        allRemindersViewController.selectedSegment = selectedSegment
-        
-        allRemindersViewController.myTabIndex = selectedViewControllerTag!
-        
         return true
     }
     
     func tabBarController(tabBarController: UITabBarController, didSelectViewController viewController: UIViewController) {
         print(#function)
-        print("Here comes the recieved message: \(sentMessage)")
-        let selectedIndex = myTabBarController.selectedIndex
-        print("Current tab is \(selectedIndex).")
-        print("I want to be tab \(myTabIndex).")
-        print("Segment is \(segmentedControl.selectedSegmentIndex)")
-        
     }
     
 }

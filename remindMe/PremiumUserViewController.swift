f92fefa6fb5a31548f452db98355de4e3ab39aa0
Duane <duane.stoltz@gmail.com>
2016-09-19 09:50:30 +0200

    set landscape premium

--- remindMe/PremiumUserViewController.swift
+++ remindMe/PremiumUserViewController.swift
@@ -30,6 +30,7 @@ class PremiumUserViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = UIColor(red: 40/255, green: 108/255, blue: 149/255, alpha: 1)
         goPremiumButton.layer.cornerRadius = 5
         restorePurchaseButton.layer.cornerRadius = 5
     }
